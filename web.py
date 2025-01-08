e45be52c75c81dc803f8de83f96dd808751882f4
tlbpython <111707906+tlbpython@users.noreply.github.com>
2025-01-08 09:10:30 -0500

    Update web.py

--- web.py
+++ web.py
@@ -22,5 +22,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="", placeholder="Add new todo ...",
+st.text_input(label="", placeholder="Add new item ...",
               on_change=add_todo, key='new_todo')
